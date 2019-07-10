94e3b0113b8b10e87583a87ab05378f6e7f9afe6
Thijs Busser <tbusser@gmail.com>
2019-07-10 20:31:21 +0200

    Add method addDays
    Adds a method will allows a provided number of days to be added or
    subtracted from a provided date.

---
+++ src/addDays/addDays.js
@@ -0,0 +1,32 @@
+import isDateLike from '../_lib/isDateLike/isDateLike';
+
+/* ========================================================================== *\
+	EXPORTS
+\* ========================================================================== */
+/**
+ * Returns a new Date instance with a date the specified number of days from the
+ * provided date.
+ *
+ * @param {Date|number} date The date to modify.
+ * @param {number} days The number of days to move from the provided date.
+ *
+ * @returns {Date} The result is a new Date instance the specified number of
+ *          days from the provided date.
+ *
+ * @throws {TypeError} A TypeError is thrown when the arguments are not of
+ *         the expected type.
+ */
+export default function addDays(date, days) {
+	if (!isDateLike(date)) {
+		throw new TypeError('The date argument is not of type Date or Number');
+	}
+	if (!Number.isInteger(days)) {
+		throw new TypeError('The days argument is not an integer number');
+	}
+
+	const
+		result = new Date(date);
+	result.setDate(result.getDate() + days);
+
+	return result;
+}
