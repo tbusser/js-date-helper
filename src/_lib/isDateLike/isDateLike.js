6c368544e39bb7bc3a3d6f5da3397f26e9b32d07
Thijs Busser <tbusser@gmail.com>
2019-07-10 20:01:06 +0200

    Add removeTime
    The removeTime method will set all time related properties to 0.

---
+++ src/_lib/isDateLike/isDateLike.js
@@ -0,0 +1,22 @@
+/* ========================================================================== *\
+	EXPORTS
+\* ========================================================================== */
+/**
+ * Tests if the provided value is date like. In this instance it means it is
+ * either an instance of Date or an instance of Number.
+ *
+ * @param {*} value The value to test
+ *
+ * @returns {boolean} True when the provided value is date like; otherwise the
+ *          result is false.
+ */
+export default function isDateLike(value) {
+	if (
+		Object.prototype.toString.call(value) !== '[object Date]' &&
+		Object.prototype.toString.call(value) !== '[object Number]'
+	) {
+		return false;
+	}
+
+	return true;
+}
---
+++ src/removeTime/removeTime.js
@@ -0,0 +1,28 @@
+import isDateLike from '../_lib/isDateLike/isDateLike';
+
+/* ========================================================================== *\
+	EXPORTS
+\* ========================================================================== */
+/**
+ * Returns a new Date instance containing only the year, month, and day of the
+ * provided date. All time related properties are set to 0.
+ *
+ * @param {Date|number} date The date from which to strip the time.
+ *
+ * @returns {Date} A new Date instance with only the year, month, and date from
+ *          the provided date.
+ *
+ * @throws {TypeError} A TypeError is thrown when the provided value is not of
+ *         type Date or Number.
+ */
+export default function removeTime(date) {
+	if (!isDateLike(date)) {
+		throw new TypeError('The date argument is not of type Date or Number');
+	}
+
+	const
+		result = new Date(date);
+	result.setHours(0, 0, 0, 0);
+
+	return result;
+}
